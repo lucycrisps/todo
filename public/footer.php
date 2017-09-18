773b446262ae64cce9a9af45517a53fefa583c37
Developers <developers@Developerss-Air.default>
2017-09-18 21:09:31 +0100

    add gitignore file

--- public/footer.php
+++ public/footer.php
@@ -6,6 +6,9 @@
 </div><!--// total-wrapper -->
 
         <script src="/assets/js/vendor/jquery-1.11.1.min.js"></script>
+        <script src="https://fb.me/react-15.1.0.js"></script>
+    	<script src="https://fb.me/react-dom-15.1.0.js"></script>
+
         <script src="/assets/js/plugins.js"></script>
         <script src="/assets/js/main.js"></script>
         
